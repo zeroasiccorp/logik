3da374ffaaee72539de5eb3a008d725f06039da5
petergrossmann21 <peter@zeroasic.com>
2024-01-17 15:27:39 -0500

    Prune unneeded build scripts

--- tools/install/install_vpr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -f
-
-#install_vpr.sh
-
-#This script installs VPR and genfasm when executed in the root directory of a
-#VPR repository clone
-
-make vpr -j$(nproc) && \
-    make genfasm -j$(nproc)
--- tools/install/install_yosys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -f
-
-#install_yosys.sh
-
-#This script installs Yosys and when executed in the root directory of a
-#Yosys repository clone
-
-make config-gcc && \
-    make -j$(nproc)
