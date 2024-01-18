33563817fafcd5493ab1f0b7c5b8da9fe5dd1a57
petergrossmann21 <peter@zeroasic.com>
2024-01-18 14:47:36 -0500

    Parameterize test across part names

--- tests/ebrick_fpga/test_adder.py
+++ tests/ebrick_fpga/test_adder.py
@@ -6,17 +6,27 @@ import siliconcompiler
 
 @pytest.mark.quick
 @pytest.mark.timeout(300)
-def test_py(setup_example_test):
+@pytest.mark.parametrize("part_name",
+                         [
+                             'zafg00um_0202',
+                             'zafg1um_0202',
+                         ])
+def test_py(setup_example_test, part_name):
     setup_example_test('adder/sc')
 
     import adder
-    adder.main()
+    adder.main(part_name=part_name)
 
 @pytest.mark.quick
 @pytest.mark.timeout(300)
-def test_cli(setup_example_test):
+@pytest.mark.parametrize("part_name",
+                         [
+                             'zafg00um_0202',
+                             'zafg1um_0202',
+                         ])
+def test_cli(setup_example_test, part_name):
     adder_dir = setup_example_test('adder/sc')
 
-    proc = subprocess.run(['bash', os.path.join(adder_dir, 'run.sh')])
+    proc = subprocess.run(['bash', os.path.join(adder_dir, 'run.sh'), part_name])
     assert proc.returncode == 0
 
