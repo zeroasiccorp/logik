b73dd7139f3a0a9e431513003fe445a549841b76
petergrossmann21 <peter@zeroasic.com>
2024-01-24 13:57:02 -0500

    Strip unnecessary functions

--- ebrick_fpga_cad/tools/fasm_to_bitstream/bitstream_finish.py
+++ ebrick_fpga_cad/tools/fasm_to_bitstream/bitstream_finish.py
@@ -27,45 +27,13 @@ def setup(chip):
 
     # Input/output requirements.
     #chip.set('tool', tool, 'task', task, 'input', design + '.fasm', step=step, index=index)
-    #chip.set('tool', tool, 'task', task, 'output', design + '_bitstream.json', step=step, index=index)
+    #chip.set('tool', tool, 'task', task, 'output', design + '_bitstream.json',
+    #         step=step, index=index)
 
     # Require that a lut size is set for FPGA scripts.
     chip.add('tool', tool, 'task', task, 'require',
              ",".join(['fpga', part_name, 'file', 'bitstream_map']),
              step=step, index=index)
-
-
-def runtime_options(chip):
-
-    part_name = chip.get('fpga', 'partname')
-
-    options = []
-
-    if chip.valid('fpga', part_name, 'file', 'bitstream_map') and \
-       chip.get('fpga', part_name, 'file', 'bitstream_map'):
-
-        bitstream_maps = chip.find_files('fpga', part_name, 'file', 'bitstream_map')
-
-    else:
-        bitstream_maps = []
-
-    topmodule = chip.top()
-    fasm = f"inputs/{topmodule}.fasm"
-
-    options.append(fasm)
-    
-    if (len(bitstream_maps) == 1):
-        options.append(bitstream_maps[0])
-    elif (len(bitstream_maps) == 0):
-        chip.error("fasm_to_bitstream.py requires a bitstream map file",
-                   fatal=True)
-    else:
-        chip.error("Only one bitstream map file can be passed to fasm_to_bitstream.py", fatal=True)
-
-    json_outfile = f"outputs/{topmodule}_bitstream.json"
-    options.append(json_outfile)
-
-    return options
 
 
 def run(chip):
@@ -95,15 +63,3 @@ def run(chip):
         chip.error("Only one bitstream map file can be passed to fasm_to_bitstream.py", fatal=True)
 
     return 0
-
-##################################################
-def pre_process(chip):
-    ''' Tool specific function to run before step execution
-    '''
-
-    step = chip.get('arg', 'step')
-    index = chip.get('arg', 'index')
-    tool, task = chip._get_tool_task(step, index)
-
-    part_name = chip.get('fpga', 'partname')
-
