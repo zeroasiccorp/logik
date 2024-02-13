b2e207013ff322616e3be17ead2e32de52bc4307
petergrossmann21 <peter@zeroasic.com>
2024-02-13 14:38:17 -0500

    Corrections so that CLI and scripted flows both execute with constraints correctly

--- examples/adder/sc/adder.py
+++ examples/adder/sc/adder.py
@@ -17,13 +17,18 @@ def main(part_name='zafg1um_0202'):
     # 1. Defining the project
 
     # 2. Define source files
-    src_path = os.path.abspath(__file__).replace('sc/adder.py','rtl')
+    project_path = os.path.abspath(__file__).replace('sc/adder.py','')
     src_files = [
-        "adder.v",
+        'adder.v',
     ]
+
+    # 3. Define constraints
+    # chip.add('input', 'constraint', 'pins', 'adder_pin_constraints.xml')
+    pinmap_file = os.path.join(project_path, 'sc', f'adder_pin_constraints_{part_name}.json')
+    chip.add('input', 'constraint', 'pinmap', pinmap_file)
     
     for filename in src_files :
-        chip.input(os.path.join(src_path, filename))
+        chip.input(os.path.join(project_path, 'rtl', filename))
 
     # 3. Load target
     chip.load_target(ebrick_fpga_target)
--- examples/adder/sc/run.sh
+++ examples/adder/sc/run.sh
@@ -3,4 +3,4 @@
 #Solution for deriving SCRIPT_DIR from
 #https://saturncloud.io/blog/how-to-get-the-directory-where-a-bash-script-is-located/
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-sc ${SCRIPT_DIR}/../rtl/adder.v -fpga_partname "$1" -flow "ebrick_fpga_flow" -target "ebrick_fpga_cad.targets.ebrick_fpga_target" -design "adder"
+sc ${SCRIPT_DIR}/../rtl/adder.v -fpga_partname "$1" -flow "ebrick_fpga_flow" -target "ebrick_fpga_cad.targets.ebrick_fpga_target" -design "adder" -input "constraint pinmap ${SCRIPT_DIR}/adder_pin_constraints_$1.json"
