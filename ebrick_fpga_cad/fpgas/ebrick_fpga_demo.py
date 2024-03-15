15cf6050cc4f0a742137a3129891ebb64394d2f5
petergrossmann21 <peter@zeroasic.com>
2024-03-15 08:43:03 -0400

    Add ebrick_fpga_demo to parts driver suite

--- ebrick_fpga_cad/fpgas/ebrick_fpga_demo.py
+++ ebrick_fpga_cad/fpgas/ebrick_fpga_demo.py
@@ -41,8 +41,8 @@ def setup(chip):
         # Assemble the name of the CAD release to obtain
         # from github
 
-        current_release = 'v0.1.2'
-        cad_part_release_url = _common.get_release_url(current_release, f'ebrick-fpga_{part_name}_cad.tar.gz')
+        current_release = 'v0.1.4'
+        cad_part_release_url = _common.get_efpga_release_url(current_release, f'{part_name}_cad.tar.gz')
         chip.register_package_source(name=f'ebrick_fpga-{part_name}',
                                      path=cad_part_release_url,
                                      ref=current_release)
@@ -95,7 +95,7 @@ def setup(chip):
             bitstream_map_file = os.path.join(cad_root, 'ebrick_fpga_core_bitstream_map.json')
             fpga.set('fpga', part_name, 'file', 'bitstream_map', bitstream_map_file)
             
-            gasket_map_file = os.path.join(cad_root, 'test_gasket_map.json')
+            gasket_map_file = os.path.join(cad_root, f'{part_name}_gasket_map.json')
             fpga.set('fpga', part_name, 'file', 'gasket_map', gasket_map_file)
             
             fpga.set('fpga', part_name, 'var', 'channelwidth', '136')
--- ebrick_fpga_cad/targets/ebrick_fpga_target.py
+++ ebrick_fpga_cad/targets/ebrick_fpga_target.py
@@ -1,4 +1,4 @@
-#zafg1um.py
+#ebrick_fpga_target.py
 
 import siliconcompiler
 from siliconcompiler.targets import utils
@@ -7,6 +7,7 @@ from siliconcompiler.targets import utils
 from ebrick_fpga_cad.fpgas import zaeg1aa
 from ebrick_fpga_cad.fpgas import zafg00um
 from ebrick_fpga_cad.fpgas import zafg1um
+from ebrick_fpga_cad.fpgas import ebrick_fpga_demo
 from ebrick_fpga_cad.flows import ebrick_fpga_flow
 
 
@@ -27,6 +28,7 @@ def setup(chip):
     chip.use(zaeg1aa)
     chip.use(zafg00um)
     chip.use(zafg1um)
+    chip.use(ebrick_fpga_demo)
 
     if part_name not in chip.getkeys('fpga'):
         chip.error(f'{part_name} has not been loaded', fatal=True)
@@ -46,4 +48,4 @@ def setup(chip):
 if __name__ == "__main__":
     target = siliconcompiler.Chip('<target>')
     setup(target)
-    target.write_manifest('zafg1um_target.json')
+    target.write_manifest('target.json')
