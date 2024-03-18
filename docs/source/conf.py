46aa425150c512913143d4c592b13d1885033047
petergrossmann21 <peter@zeroasic.com>
2024-03-18 14:48:07 -0400

    Start updating names

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,10 +11,10 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Switchboard'
-copyright = '2023, Zero ASIC'
+project = '<Name>'
+copyright = '2024, Zero ASIC'
 author = 'Zero ASIC'
-release = '0.0.29'
+release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
