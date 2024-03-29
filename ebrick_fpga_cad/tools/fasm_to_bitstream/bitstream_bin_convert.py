5e61542161c8cadb88084b351d480f7323d0b9ef
petergrossmann21 <peter@zeroasic.com>
2024-03-29 16:18:36 -0400

    Draft binary to ASCII flattened bitstream converter

---
+++ ebrick_fpga_cad/tools/fasm_to_bitstream/bitstream_bin_convert.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def main():
+
+    option_parser = argparse.ArgumentParser()
+
+    option_parser.add_argument("binary_bitstream",
+                               help="binary bitstream file")
+    option_parser.add_argument("ascii_bitstream",
+                               help="ascii bitstream file")
+
+    options = option_parser.parse_args()
+
+    bin_filename = options.binary_bitstream
+    out_filename = options.ascii_bitstream
+
+    words = []
+    with open(bin_filename, 'rb') as bin_file:
+        for cur_byte in bin_file.read():
+            formatted_word = format(cur_byte, "0x").zfill(2)
+            words.append(formatted_word)
+
+    with open(out_filename, 'w') as out_file:
+        for word in words:
+            out_file.write(word)
+            out_file.write('\n')
+
+
+if __name__ == "__main__":
+    main()
