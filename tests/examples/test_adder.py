ffad2358474247d1db0b7e44db79e30bc492a949
petergrossmann21 <peter@zeroasic.com>
2024-04-02 13:17:16 -0400

    Adjust adder pytest to fit with simplified example

--- tests/examples/test_adder.py
+++ tests/examples/test_adder.py
@@ -5,26 +5,16 @@ import pytest
 
 
 @pytest.mark.timeout(300)
-@pytest.mark.parametrize("part_name",
-                         [
-                             'logik_demo',
-                         ])
-def test_py(setup_example_test, part_name):
+def test_py(setup_example_test):
     setup_example_test('adder')
 
     import adder
-    adder.main(part_name=part_name)
+    adder.hello_adder()
 
 
 @pytest.mark.timeout(300)
-@pytest.mark.parametrize("part_name",
-                         [
-                             'logik_demo',
-                         ])
-def test_cli(setup_example_test, part_name):
+def test_cli(setup_example_test):
     adder_dir = setup_example_test('adder')
 
-    proc = subprocess.run([os.path.join(adder_dir, 'adder.py'),
-                           '-fpga_partname',
-                           part_name])
+    proc = subprocess.run([os.path.join(adder_dir, 'adder.py')])
     assert proc.returncode == 0
