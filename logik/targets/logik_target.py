d1515345eaa835623fad905cfd530e02a80a016e
Peter Gadfort <gadfort@zeroasic.com>
2024-07-26 11:35:45 -0400

    fix logik target

--- logik/targets/logik_target.py
+++ logik/targets/logik_target.py
@@ -1,7 +1,7 @@
 # Copyright 2024 Zero ASIC Corporation
 # Licensed under the MIT License (see LICENSE for details)
 
-import siliconcompiler
+from siliconcompiler import Chip, SiliconCompilerError
 
 from logik.fpgas import logik_demo
 from logik.flows import logik_flow
@@ -18,13 +18,13 @@ def setup(chip):
     # 1. Configure fpga part
     part_name = chip.get('fpga', 'partname')
     if not part_name:
-        chip.error('FPGA partname has not been set.', fatal=True)
+        raise ValueError('FPGA partname has not been set.')
 
     # 2.  Load all available FPGAs
     chip.use(logik_demo)
 
     if part_name not in chip.getkeys('fpga'):
-        chip.error(f'{part_name} has not been loaded', fatal=True)
+        raise SiliconCompilerError(f'{part_name} has not been loaded')
 
     # 3. Load flow
     chip.use(logik_flow)
@@ -35,6 +35,6 @@ def setup(chip):
 
 #########################
 if __name__ == "__main__":
-    target = siliconcompiler.Chip('<target>')
+    target = Chip('<target>')
     setup(target)
     target.write_manifest('target.json')
