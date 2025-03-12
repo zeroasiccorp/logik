078bbd2464893a69cab0f48805a7ed8d0dd974d5
petergrossmann21 <peter@zeroasic.com>
2025-03-12 14:53:32 -0400

    Add hooks to test example archs with adder to show that each is ready for use

--- examples/adder/adder.py
+++ examples/adder/adder.py
@@ -5,14 +5,17 @@
 
 from siliconcompiler import Chip
 from logik.flows import logik_flow
+from logiklib.demo.example_arch_X005Y005 import example_arch_X005Y005
+from logiklib.demo.example_arch_X008Y008 import example_arch_X008Y008
+from logiklib.demo.example_arch_X014Y014 import example_arch_X014Y014
+from logiklib.demo.example_arch_X030Y030 import example_arch_X030Y030
 from logiklib.demo.logik_demo import logik_demo
 
 
-def hello_adder():
+def hello_adder(part_name='logik_demo'):
 
     # Create compilation object
     chip = Chip('adder')
-    chip.create_cmdline(switchlist=['-remote'])
 
     # Specify design sources
     chip.input('adder.v')
@@ -24,12 +27,27 @@ def hello_adder():
     chip.set('option', 'quiet', True)
 
     # Select target fpga
-    chip.set('fpga', 'partname', 'logik_demo')
+    chip.set('fpga', 'partname', part_name)
+
+    chip.set('option', 'builddir', f'adder_{part_name}')
 
     # Load target settings
     chip.set('option', 'flow', 'logik_flow')
     chip.use(logik_flow)
-    chip.use(logik_demo)
+    # Only use the part you're planning to use; otherwise
+    # all the CAD tarballs get downloaded
+    if (part_name == 'example_arch_X005Y005'):
+        chip.use(example_arch_X005Y005)
+    elif (part_name == 'example_arch_X008Y008'):
+        chip.use(example_arch_X008Y008)
+    elif (part_name == 'example_arch_X014Y014'):
+        chip.use(example_arch_X014Y014)
+    elif (part_name == 'example_arch_X030Y030'):
+        chip.use(example_arch_X030Y030)
+    elif (part_name == 'logik_demo'):
+        chip.use(logik_demo)
+    else:
+        raise ValueError(f"Unsupported part name {part_name}")
 
     # Run compiler
     chip.run()
--- tests/examples/test_adder.py
+++ tests/examples/test_adder.py
@@ -18,6 +18,21 @@ def test_py(setup_example_test, monkeypatch):
 
 
 @pytest.mark.timeout(300)
+@pytest.mark.parametrize("part_name",
+                         ['example_arch_X005Y005',
+                          'example_arch_X008Y008',
+                          'example_arch_X014Y014',
+                          'example_arch_X030Y030'])
+def test_example_parts(part_name, setup_example_test, monkeypatch):
+    adder_dir = setup_example_test('adder')
+
+    monkeypatch.chdir(adder_dir)
+
+    import adder
+    adder.hello_adder(part_name=part_name)
+
+
+@pytest.mark.timeout(300)
 def test_cli(setup_example_test):
     adder_dir = setup_example_test('adder')
 
